99f93c4a3b6171dc6aaa4991dda630a44fae2b4f
Joseph Murillo <josephmurillo2425@gmail.com>
2021-01-29 14:46:58 -0600

    Arrays and methods

---
+++ 19_arrays.rb
@@ -0,0 +1,12 @@
+my_array = []
+my_array.push(4)
+
+print my_array
+puts
+my_array.push(100)
+print my_array
+puts
+puts my_array[1]
+my_array.push("hola", true)
+print my_array
+puts
---
+++ 21_methods.rb
@@ -0,0 +1,9 @@
+def twice(thing)
+    thing + thing
+end
+
+test_var = twice(4)
+puts test_var
+
+test_var2 = twice([2, 4, 6])
+puts test_var2
